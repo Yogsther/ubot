12dc9e2434e8574aca11ed4fcb55d3fe675bda8b
Olle Kaiser <olle.kaiser@goldtowngames.com>
2025-03-28 22:51:21 +0100

    Character & KCC

--- ubot-game/Assets/QuantumUser/Simulation/Frame.User.cs
+++ ubot-game/Assets/QuantumUser/Simulation/Frame.User.cs
@@ -2,8 +2,17 @@
 {
     public unsafe partial class Frame
     {
+
+		public Config Config
+		{
+			get
+			{
+				return this.FindAsset<Config>(this.RuntimeConfig.Config.Id);
+			}
+		}
+
 #if UNITY_ENGINE
 
 #endif
-    }
+	}
 }
