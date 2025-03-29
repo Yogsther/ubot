abb9f9bb01b9e7d3e48bfc8ed717108956fc1113
Wille Pilquist <digitalmoleguy@gmail.com>
2025-03-29 15:38:34 +0100

    spawning subs + distance fade

--- ubot-game/Assets/QuantumUser/Simulation/Assets/Config.cs
+++ ubot-game/Assets/QuantumUser/Simulation/Assets/Config.cs
@@ -3,5 +3,6 @@ namespace Quantum
 	public partial class Config : AssetObject {
 
 		public AssetRef<EntityPrototype> PlayerPrototype;
+		public AssetRef<EntityPrototype> SubmarinePrototype;
 	}
 }
--- ubot-game/Assets/QuantumUser/Simulation/Systems/GameSystem.cs
+++ ubot-game/Assets/QuantumUser/Simulation/Systems/GameSystem.cs
@@ -1,14 +1,16 @@
+using Photon.Deterministic;
 using UnityEngine;
 using UnityEngine.Scripting;
 
 namespace Quantum
 {
 	[Preserve]
-	public class GameSystem : SystemMainThread, ISignalOnPlayerAdded
+	public unsafe class GameSystem : SystemMainThread, ISignalOnPlayerAdded
 	{
 		public override void OnInit(Frame f)
 		{
-			
+			SpawnSubmarine(f, TeamRef.Attacker, new FPVector3(FP._200, FP._0, FP._200));
+			SpawnSubmarine(f, TeamRef.Defender, new FPVector3(-FP._200, FP._0, -FP._200));
 		}
 
 		public void OnPlayerAdded(Frame f, PlayerRef player, bool firstTime)
@@ -26,6 +28,21 @@ namespace Quantum
 
 		public override void Update(Frame f)
 		{
+		}
+
+		private void SpawnSubmarine(Frame f, TeamRef team, FPVector3 position)
+		{
+			var submarineEntity = f.Create(f.Config.SubmarinePrototype);
+
+			TeamLink teamLink = new TeamLink();
+			teamLink.Team = team;
+
+			f.Add(submarineEntity, teamLink);
+
+			if (f.Unsafe.TryGetPointer(submarineEntity, out Transform3D* transform))
+			{
+				transform->Position = position;
+			}
 		}
 	}
 }
